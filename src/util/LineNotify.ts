d7b8cee9ce32c2c2bbf31f1118437298a7779d0b
Watchara <rockdevper@gmail.com>
2026-02-22 17:32:11 +0700

    support LineMessaging API and Telegram

---
+++ src/util/LineNotify.ts
@@ -0,0 +1,36 @@
+/*
+  # Author : Watchara Pongsri
+  # [github/X-c0d3] https://github.com/X-c0d3/
+  # Web Site: https://www.rockdevper.com
+*/
+
+import axios from 'axios';
+import { AppConfig } from '../constants/Constants';
+
+const sendLineNotify = async (message: String) => {
+  const userIds = (AppConfig.LINE_SENDER_ID || '')
+    .split(/[,]/)
+    .map((s) => s.trim())
+    .filter(Boolean);
+
+  await axios({
+    method: 'post',
+    url: 'https://api.line.me/v2/bot/message/multicast',
+    timeout: 5000,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${AppConfig.LINE_TOKEN}`,
+    },
+    data: {
+      to: userIds,
+      messages: [
+        {
+          type: 'text',
+          text: message,
+        },
+      ],
+    },
+  }).catch((err) => console.log(err));
+};
+
+export { sendLineNotify };
---
+++ src/util/TelegramNotify.ts
@@ -0,0 +1,27 @@
+/*
+  # Author : Watchara Pongsri
+  # [github/X-c0d3] https://github.com/X-c0d3/
+  # Web Site: https://www.rockdevper.com
+*/
+
+import axios from 'axios';
+import { AppConfig } from '../constants/Constants';
+
+const sendTelegramNotify = async (message: String) => {
+  console.log(message);
+  await axios({
+    method: 'post',
+    url: `https://api.telegram.org/bot${AppConfig.TELEGRAM_API_KEY}/sendMessage`,
+    timeout: 5000,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    data: {
+      chat_id: AppConfig.TELEGRAM_CHAT_ID,
+      text: message,
+      parse_mode: 'HTML',
+    },
+  }).catch((err) => console.log(err));
+};
+
+export { sendTelegramNotify };
